a780961fad128dd574385921a36049dfa95d69d8
Nacho <azurestyle@gmail.com>
2026-09-14 15:41:25 +0000

    New module Real Estate

---
+++ real_estate_nacho/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Real Estate Nacho",
+    "version": "1.0",
+    "summary": "Real estate management module",
+    "description": "Module to manage real estate properties, agents, and clients.",
+    "author": "Nacho",
+    "category": "Real Estate",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/realestate_property_views.xml", 
+        "views/realestate_menuitems.xml"],
+    "installable": True,
+    "application": True,
+}
