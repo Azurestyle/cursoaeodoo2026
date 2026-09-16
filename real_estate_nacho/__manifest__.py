eec414c402af136c9668b1188e7b4d0709741d3d
Nacho <azurestyle@gmail.com>
2026-09-16 15:48:59 +0000

    Vistas de listado y form

--- real_estate_nacho/__manifest__.py
+++ real_estate_nacho/__manifest__.py
@@ -7,6 +7,7 @@
     "category": "Real Estate",
     "depends": ["base"],
     "data": [
+        "security/real_estate_security.xml",
         "security/ir.model.access.csv",
         "views/realestate_property_views.xml", 
         "views/realestate_menuitems.xml"],
